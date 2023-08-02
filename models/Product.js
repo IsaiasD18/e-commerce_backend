37bd186be6ce181ae3497efbafd50565f388e01c
IsaiasD18 <133791406+IsaiasD18@users.noreply.github.com>
2023-08-01 22:30:02 -0400

    Defined the columns for the models and set up the models association

--- models/Product.js
+++ models/Product.js
@@ -10,6 +10,44 @@ class Product extends Model {}
 Product.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    price: {
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+      validate: {
+        isDecimal: {
+          args: true,
+          msg: 'Price must be a valid decimal number.',
+        }
+      }
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 10,
+      validate: {
+        isNumeric: {
+          args: true,
+          msg: 'Stock must be a valid number.',
+        }
+      }
+    },
+    category_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'category',
+        key: 'id'
+      }
+    }
   },
   {
     sequelize,
